eb2cac762c46f9b91bf82d6bcf3f9de09c316610
Sanket-Mengal <105072822+SanketMengal@users.noreply.github.com>
2025-07-30 10:08:11 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
         return "<html><body>" +
                "<img src='https://media.licdn.com/dms/image/v2/D4D12AQHFnWsqtTvJRA/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1735213222529?e=1759363200&v=beta&t=YWt1-ieh3t9OalOgDkChBsQnUWUoWS1_nkanyFli6Ls' " +
                "style='max-width:100%; height:auto;' alt='Cover Image'>" +
-               "<p><b>Hello from Sanket</b></p>" +
+               "<p><b>Hello, Sanket for Pune</b></p>" +
                "</body></html>";
     }
 }
