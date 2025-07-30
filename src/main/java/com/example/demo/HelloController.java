a72b453f5d9b942842318c6f6167dfa3d5d533ba
Sanket-Mengal <105072822+SanketMengal@users.noreply.github.com>
2025-07-30 09:36:20 +0530

    Image updated HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,3 +1,5 @@
+// File: src/main/java/com/example/demo/HelloController.java
+
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,8 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping(value = "/", produces = "text/html")
     public String hello() {
-        return "Hello From sanket!";
+        return "<html><body>" +
+               "<img src='https://media.licdn.com/dms/image/v2/D4D12AQHFnWsqtTvJRA/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1735213222529?e=1759363200&v=beta&t=YWt1-ieh3t9OalOgDkChBsQnUWUoWS1_nkanyFli6Ls' " +
+               "style='max-width:100%; height:auto;' alt='Cover Image'>" +
+               "<p><b>Hello from Sanket</b></p>" +
+               "</body></html>";
     }
 }
